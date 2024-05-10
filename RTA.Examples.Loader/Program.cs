e6079149dfc4386ef9219c2a0741bf80cdeeb025
MykolaAtMcLarenApplied <146206549+MykolaAtMcLarenApplied@users.noreply.github.com>
2024-05-10 13:53:52 +0100

    made changes to populate DB with a sample set of sessions, whcih has a sample set of markers/laps to make them eventually visible in A10 (#3)
    * made changes to populate DB with a sample set of sessions, whcih has a sample set of markers/laps to make them eventually visible in A10
    
    * addressed PR feadback: added some comments to the code
    
    * a small correction
    
    * addressing another feedback from Nikhil and Owen

--- RTA.Examples.Loader/Program.cs
+++ RTA.Examples.Loader/Program.cs
@@ -3,11 +3,12 @@
 
 using System;
 using System.Runtime.InteropServices;
-using System.Threading;
 using System.Threading.Tasks;
+
 using Google.Protobuf;
 using Grpc.Core;
 using Grpc.Net.Client;
+
 using MAT.OCS.Configuration;
 using MAT.OCS.Configuration.Builder;
 using MAT.OCS.RTA.Model.Data;
@@ -16,6 +17,7 @@ using MAT.OCS.RTA.Toolkit.API.ConfigService;
 using MAT.OCS.RTA.Toolkit.API.DataService;
 using MAT.OCS.RTA.Toolkit.API.SessionService;
 using RTA.Examples.Util;
+using static MAT.OCS.RTA.Toolkit.API.SessionService.SessionUpdate.Types;
 
 namespace RTA.Examples.Loader
 {
@@ -23,6 +25,16 @@ namespace RTA.Examples.Loader
     {
         private static readonly string[] Fields = {"alpha", "beta", "gamma"};
 
+        //-----------------------------------------------------------------------------------------------------------------------------
+        // This program is an example of how to create few records of RTA sessions in the Postgres DB by utilizing the OCS.RTA.Toolkit,
+        // namely its Rest Api web services, which write the sample data into the DB and associated 'data chuncks' files.
+        // The data in question are RTA sessions and its Laps (called 'markers' here), which are linked to each sessions.
+        // They are handled by using SessionStore service. See also WriteSessionAsync().
+        // It also creates a session configuration data, which is stored in a Json file.
+        // This is handled by ConfigStore service. See also WriteConfigAsync() method.
+        // Finally, it generates sample 'data chuncks' files to simulates the data which represents session parameters.
+        // This is handeld by the DataWriter. See also WriteDataAsync() method.
+        //-----------------------------------------------------------------------------------------------------------------------------
         public static async Task Main(string[] args)
         {
             using var channel = GrpcChannel.ForAddress("http://localhost:8082");
@@ -30,23 +42,30 @@ namespace RTA.Examples.Loader
             var configClient = new ConfigStore.ConfigStoreClient(channel);
             var dataClient = new DataWriter.DataWriterClient(channel);
 
-            var dataIdentity = Guid.NewGuid().ToString();
             var timestamp = DateTimeOffset.Now;
             var startNanos = (timestamp.ToUniversalTime() - DateTimeOffset.UnixEpoch).Ticks * 100;
             var durationNanos = TimeSpan.FromMinutes(10).Ticks * 100;
             var intervalNanos = TimeSpan.FromMilliseconds(10).Ticks * 100;
 
-            await WriteDataAsync(
-                dataClient, dataIdentity, startNanos, durationNanos, intervalNanos);
+            var configIdentifier = await WriteConfigAsync(configClient, intervalNanos);
 
-            var configIdentifier = await WriteConfigAsync(
-                configClient, intervalNanos);
+            // At this point we create a set of sample sessions:
+            // 1) creates 9 sessions (where '9' is an arbitrary constant, good enough for testing purpose);
+            // 2) creates 'data chuncks' files assiciated with each session;
+            // 3) and links the lap/marker sample data for each session (this is done inside of WriteSessionAsync() call)
+            //
+            const int sessionInTotal = 9;
+            for (int sessionNumber = 1; sessionNumber <= sessionInTotal; sessionNumber++)
+            {
+                var dataIdentity = Guid.NewGuid().ToString();
 
-            await WriteSessionAsync(
-                sessionClient, dataIdentity,
-                timestamp, startNanos, durationNanos, intervalNanos, configIdentifier);
+                await WriteDataAsync(dataClient, dataIdentity, startNanos, durationNanos, intervalNanos);
 
-            Console.WriteLine(dataIdentity);
+                await WriteSessionAsync(sessionClient, dataIdentity, timestamp, startNanos, durationNanos, intervalNanos,
+                                        configIdentifier, sessionNumber);
+
+                Console.WriteLine(dataIdentity);
+            }
         }
 
         private static async Task WriteDataAsync(
@@ -58,45 +77,54 @@ namespace RTA.Examples.Loader
         {
             var rng = new Random();
             var signals = new SignalGenerator[Fields.Length];
+
             for (var f = 0; f < signals.Length; f++)
+            {
                 signals[f] = new SignalGenerator(rng);
+            }
 
             const int burstLength = 100;
             var burstSamples = new short[burstLength];
 
             using var dataStream = dataClient.WriteDataStream();
+
             var requestStream = dataStream.RequestStream;
             var responseStream = dataStream.ResponseStream;
 
-            await requestStream.WriteAsync(new WriteDataStreamMessage
-            {
-                DataIdentity = dataIdentity
-            });
+            await requestStream.WriteAsync
+                (new WriteDataStreamMessage
+                    {
+                        DataIdentity = dataIdentity
+                    }
+                );
 
             for (var offset = 0L; offset < durationNanos; offset += burstLength * intervalNanos)
             {
                 for (var f = 0; f < signals.Length; f++)
                 {
                     var signal = signals[f];
+
                     for (var t = 0; t < burstLength; t++)
                     {
                         var timeOffsetNanos = offset + t * intervalNanos;
-                        burstSamples[t] = (short) signal[timeOffsetNanos]; // short precision for this demo
+                        burstSamples[t] = (short)signal[timeOffsetNanos]; // short precision for this demo
                     }
 
                     var burst = new PeriodicData
                     {
-                        ChannelId = (uint) f,
+                        ChannelId = (uint)f,
                         StartTimestamp = startNanos + offset,
                         Interval = intervalNanos,
                         Samples = burstLength,
                         Buffer = ByteString.CopyFrom(MemoryMarshal.AsBytes(burstSamples.AsSpan()))
                     };
 
-                    await requestStream.WriteAsync(new WriteDataStreamMessage
-                    {
-                        PeriodicData = burst
-                    });
+                    await requestStream.WriteAsync
+                        (new WriteDataStreamMessage
+                            {
+                                PeriodicData = burst
+                            }
+                        );
                 }
 
                 if ((offset % 1_000_000_000L) == 0)
@@ -127,12 +155,11 @@ namespace RTA.Examples.Loader
 
             for (var f = 0; f < Fields.Length; f++)
             {
-                channels[f] = new ChannelBuilder((uint) f, intervalNanos, DataType.Signed16Bit,
-                    ChannelDataSource.Periodic);
+                channels[f] = new ChannelBuilder((uint)f, intervalNanos, DataType.Signed16Bit, ChannelDataSource.Periodic);
 
                 parameters[f] = new ParameterBuilder($"{Fields[f]}:demo", Fields[f], $"{Fields[f]} field")
                 {
-                    ChannelIds = {(uint) f},
+                    ChannelIds = {(uint)f},
                     MinimumValue = -1500,
                     MaximumValue = +1500
                 };
@@ -165,46 +192,103 @@ namespace RTA.Examples.Loader
             long startNanos,
             long durationNanos,
             long intervalNanos,
-            string configIdentifier)
+            string configIdentifier,
+            int sessionNumber)
         {
-            await sessionClient.CreateOrUpdateSessionAsync(new CreateOrUpdateSessionRequest
+            var lapChunk = durationNanos / (sessionNumber + 1);            // 1e9 * 60 = 60000000000
+
+            // This part implements how to add Laps/Markers to each session.
+            // In this change:
+            //  -- each session has an increasing number of markers, i.e. 2, 3, 4, 5, ... etc.
+            //  -- each lap/marker has different id and label values correspondingly.
+            //  -- the StartTime and EndTime of each marker is calculated in the way that the laps are evenly divided for the whole timeline
+            //     (again, this is merely for 'facilitating' testing in A10, when a session is loaded)
+            //  -- the markers firstly are generated as a list of them, and then the list is added to the session in question.
+            // 
+            var newMarkerList = new MarkersList();
+
+            for (int i = 1;  i <= sessionNumber; i++)
             {
-                Identity = dataIdentity,
-                CreateIfNotExists = new()
+                var m1 = new Marker
                 {
-                    Identifier = $"Data Service Demo {timestamp:f}",
-                    Timestamp = timestamp.ToString("O")
-                },
-                Updates =
-                {
-                    new SessionUpdate
+                    Id = i.ToString(),
+                    Type = "Test",
+                    Label = "Test Label" + i.ToString(),
+                    StartTime = startNanos + (i - 1) * lapChunk,
+                    EndTime = startNanos + i * lapChunk,
+                    DetailsJson = ""
+                };
+                newMarkerList.Markers.Add(m1);
+            }
+            var m2 = new Marker
+            {
+                Id = (sessionNumber + 1).ToString(),
+                Type = "Test",
+                Label = "Test Label" + (sessionNumber + 1).ToString(),
+                StartTime = startNanos + sessionNumber * lapChunk,
+                EndTime = startNanos + (sessionNumber + 1) * lapChunk,
+                DetailsJson = ""
+            };
+            newMarkerList.Markers.Add(m2);
+
+            await sessionClient.CreateOrUpdateSessionAsync
+                (
+                    new CreateOrUpdateSessionRequest
                     {
-                        SetType = "rta"
-                    },
-                    new SessionUpdate
-                    {
-                        // inclusive timestamps
-                        SetTimeRange = new()
+                        Identity = dataIdentity,
+                        CreateIfNotExists = new()
                         {
-                            StartTime = startNanos,
-                            EndTime = startNanos + durationNanos - intervalNanos
-                        }
-                    },
-                    new SessionUpdate
-                    {
-                        SetConfigBindings = new()
+                            Identifier = $"Session {sessionNumber} for Data Service Demo {timestamp:f}",
+                            Timestamp = timestamp.ToString("O")
+                        },
+                        Updates =
                         {
-                            ConfigBindings =
+                            new SessionUpdate
                             {
-                                new ConfigBinding
+                                SetType = "rta"
+                            },
+                            new SessionUpdate
+                            {
+                                // inclusive timestamps
+                                SetTimeRange = new()
                                 {
-                                    Identifier = configIdentifier
+                                    StartTime = startNanos,
+                                    EndTime = startNanos + durationNanos - intervalNanos
+                                }
+                            },
+                            new SessionUpdate
+                            {
+                                SetConfigBindings = new()
+                                {
+                                    ConfigBindings =
+                                    {
+                                        new ConfigBinding
+                                        {
+                                            Identifier = configIdentifier
+                                        }
+                                    }
+                                }
+                            },
+                            new SessionUpdate
+                            {
+                                UpdateMarkers = new SessionUpdate.Types.MarkersMap
+                                {
+                                    MarkersByCategory =
+                                    {
+                                        ["Laps"] = newMarkerList
+                                    }
+                                }
+                            },
+                            new SessionUpdate
+                            {
+                                UpdateDetailsJson = new DetailsJson
+                                {
+                                    Json = "{\"Number\":" + sessionNumber+"}"
                                 }
                             }
-                        }
+                        },
                     }
-                }
-            });
+                );
         }
     }
 }
